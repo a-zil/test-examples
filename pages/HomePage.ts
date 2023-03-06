f01f7e6a553d27c9112c7c14322b480c21685edc
a-zil <askhat.zilmukhamedov@gmail.com>
2023-03-06 11:02:47 -0700

    reconfigured prettier

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -1,26 +1,26 @@
-import { expect, Page, Locator } from "@playwright/test";
+import { expect, Page, Locator } from '@playwright/test'
 
 export class HomePage {
-    readonly page: Page;
-    readonly featuresItemsHeading: Locator;
-    readonly loginNavLink: Locator;
+  readonly page: Page
+  readonly featuresItemsHeading: Locator
+  readonly loginNavLink: Locator
 
-    constructor(page) {
-        this.page = page;
-        this.featuresItemsHeading = this.page.locator(".features_items .title");
-        this.loginNavLink = this.page.locator(".nav a[href='/login']");
-    }
+  constructor(page) {
+    this.page = page
+    this.featuresItemsHeading = this.page.locator('.features_items .title')
+    this.loginNavLink = this.page.locator(".nav a[href='/login']")
+  }
 
-    async navigate() {
-        await this.page.goto("https://automationexercise.com/");
-    }
+  async navigate() {
+    await this.page.goto('https://automationexercise.com/')
+  }
 
-    async assertHomePageToBeVisisble() {
-        await expect(this.featuresItemsHeading).toHaveText("Features Items");
-        await expect(this.featuresItemsHeading).toBeVisible();
-    }
+  async assertHomePageToBeVisisble() {
+    await expect(this.featuresItemsHeading).toHaveText('Features Items')
+    await expect(this.featuresItemsHeading).toBeVisible()
+  }
 
-    async navigateToLoginPage() {
-        await this.loginNavLink.click();
-    }
+  async navigateToLoginPage() {
+    await this.loginNavLink.click()
+  }
 }
--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -1,32 +1,30 @@
-import { expect, Page, Locator } from "@playwright/test";
+import { expect, Page, Locator } from '@playwright/test'
 
 export class LoginPage {
-    readonly page: Page;
-    readonly signUpFormHeading: Locator;
-    readonly signUpNameInput: Locator;
-    readonly signUpEmailInput: Locator;
-    readonly signUpButton: Locator;
+  readonly page: Page
+  readonly signUpFormHeading: Locator
+  readonly signUpNameInput: Locator
+  readonly signUpEmailInput: Locator
+  readonly signUpButton: Locator
 
-    constructor(page) {
-        this.page = page;
-        this.signUpFormHeading = this.page.locator(".signup-form h2");
-        this.signUpNameInput = this.page.locator(
-            ".signup-form input[name='name']"
-        );
-        this.signUpEmailInput = this.page.locator(
-            ".signup-form input[name='email']"
-        );
-        this.signUpButton = this.page.locator(".signup-form button");
-    }
+  constructor(page) {
+    this.page = page
+    this.signUpFormHeading = this.page.locator('.signup-form h2')
+    this.signUpNameInput = this.page.locator(".signup-form input[name='name']")
+    this.signUpEmailInput = this.page.locator(
+      ".signup-form input[name='email']"
+    )
+    this.signUpButton = this.page.locator('.signup-form button')
+  }
 
-    async assertNewUserSignUpToBeVisible() {
-        await expect(this.signUpFormHeading).toHaveText("New User Signup!");
-        await expect(this.signUpFormHeading).toBeVisible();
-    }
+  async assertNewUserSignUpToBeVisible() {
+    await expect(this.signUpFormHeading).toHaveText('New User Signup!')
+    await expect(this.signUpFormHeading).toBeVisible()
+  }
 
-    async submitSignUpForm(name: string, email: string) {
-        await this.signUpNameInput.fill(name);
-        await this.signUpEmailInput.fill(email);
-        await this.signUpButton.click();
-    }
+  async submitSignUpForm(name: string, email: string) {
+    await this.signUpNameInput.fill(name)
+    await this.signUpEmailInput.fill(email)
+    await this.signUpButton.click()
+  }
 }
